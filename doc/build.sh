e9b4f6e4afcb4e769f0bd085b86a92ae57d8c536
Peter Brittain <peter.brittain.os@gmail.com>
2015-04-16 23:50:24 +0100

    Remove irritating undocumented methods.

--- doc/build.sh
+++ doc/build.sh
@@ -1,2 +1,4 @@
 PYTHONPATH=.. sphinx-apidoc ../asciimatics -o ./source -f
+cat source/asciimatics.rst | grep -v "undoc" > source/tmp.rst
+mv -f source/tmp.rst source/asciimatics.rst
 PYTHONPATH=.. sphinx-build -b html ./source ./build
