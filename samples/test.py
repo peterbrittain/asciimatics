6ef6f6263c8c92b9e85e1b8ef69250b674970215
Peter Brittain <peter.brittain.os@gmail.com>
2015-04-18 10:13:55 +0100

    Add test sample as used in readme.

---
+++ samples/test.py
@@ -0,0 +1,22 @@
+import curses
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+
+def demo(win):
+    screen = Screen(win)
+    effects = [
+        Cycle(
+            screen,
+            FigletText("ASCIIMATICS", font='big'),
+            screen.height / 2 - 8),
+        Cycle(
+            screen,
+            FigletText("ROCKS!", font='big'),
+            screen.height / 2 + 3),
+        Stars(screen, 200)
+    ]
+    screen.play([Scene(effects, 500)])
+
+curses.wrapper(demo)
