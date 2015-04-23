437bd580004a4107fc2d3869255fddfba093db60
peterbrittain <pab_user@server.fake>
2015-04-23 12:20:04 +0100

    Updates for new release.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='asciimatics',
-    version='0.2.0',
+    version='0.3.0',
     description='An ASCII art and text cinematic storyboard/demo library',
     long_description=long_description,
     url='https://github.com/peterbrittain/asciimatics.git',
