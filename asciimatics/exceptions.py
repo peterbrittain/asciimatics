5193963e0f5284a5dbed223233fa75cb928f5241
peterbrittain <peterbrittain@users.noreply.github.com>
2015-05-21 00:21:28 +0100

    More compatibility checks for python 2/3

--- asciimatics/exceptions.py
+++ asciimatics/exceptions.py
@@ -1,3 +1,8 @@
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+
+
 class ResizeScreenError(Exception):
     """
     Asciimatics raises this Exception if the terminal is resized while playing
