d30be58fc8a21592c84f553a2a4c041b5be38bcf
Peter Brittain <peter.brittain.os@gmail.com>
2015-06-13 16:20:55 +0100

    Fleshed out BarChart support and demo.

--- samples/bars.py
+++ samples/bars.py
@@ -9,7 +9,7 @@ import time
 from random import randint
 
 def fn():
-    return randint(0, 34)
+    return randint(0, 40)
 
 
 def demo(screen):
@@ -26,15 +26,47 @@ def demo(screen):
     scenes.append(Scene(effects, 300))
 
     def wv(x):
-        return lambda: int(27 * (1 + math.sin(math.pi * (2*time.time()+x) / 5)))
+        return lambda: 1 + math.sin(math.pi * (2*time.time()+x) / 5)
 
     effects = [
         Print(screen,
               BarChart(
-                  12, 60,
+                  13, 60,
                   [wv(1), wv(2), wv(3), wv(4), wv(5), wv(7), wv(8), wv(9)],
-                  colour=[c for c in range(1,8)]),
-              (screen.height - 12) // 2, transparent=False, speed=2)
+                  colour=Screen.COLOUR_GREEN,
+                  axes=BarChart.BOTH,
+                  scale=2.0),
+              (screen.height - 13) // 2, transparent=False, speed=2)
+    ]
+    scenes.append(Scene(effects, 300))
+
+    effects = [
+        Print(screen,
+              BarChart(
+                  7, 60, [lambda: time.time() * 10 % 101],
+                  gradient=[(10, 234), (20, 236), (30, 238), (40, 240),
+                            (50, 242), (60, 244), (70, 246), (80, 248),
+                            (90, 250), (100, 252)],
+                  char=">",
+                  scale=100.0,
+                  labels=True,
+                  axes=BarChart.X_AXIS),
+              (screen.height - 7) // 2, transparent=False, speed=2)
+    ]
+    scenes.append(Scene(effects, 300))
+
+    effects = [
+        Print(screen,
+              BarChart(
+                  10, 60,
+                  [wv(1), wv(2), wv(3), wv(4), wv(5), wv(7), wv(8), wv(9)],
+                  colour=[c for c in range(1,8)],
+                  scale=2.0,
+                  axes=BarChart.X_AXIS,
+                  intervals=0.25,
+                  labels=True,
+                  border=False),
+              (screen.height - 10) // 2, transparent=False, speed=2)
     ]
     scenes.append(Scene(effects, 300))
 
