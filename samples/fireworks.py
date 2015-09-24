d1ccaff63e8ab7bc21509c073175101043b08bf4
Peter Brittain <peter.brittain.os@gmail.com>
2015-09-24 18:02:03 +0100

    Fix up new pep-8 warnings and more particle effects for fireworks.

--- samples/fireworks.py
+++ samples/fireworks.py
@@ -1,10 +1,10 @@
 from asciimatics.effects import Stars, Print
-from asciimatics.particles import Firework
+from asciimatics.particles import RingFirework, SerpentFirework, StarFirework
 from asciimatics.renderers import SpeechBubble
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError
-from random import randint
+from random import randint, choice
 import sys
 
 
@@ -17,12 +17,18 @@ def demo(screen):
               y=screen.height // 2 - 1,
               start_frame=300)
     ]
-    for _ in range(40):
+    for i in range(80):
+        fireworks = [
+            (RingFirework, 5, 20),
+            (StarFirework, 5, 20),
+            (SerpentFirework, 20, 25),
+        ]
+        firework, start, stop = choice(fireworks)
         effects.insert(1,
-                       Firework(screen,
+                       firework(screen,
                                 randint(0, screen.width),
                                 randint(0, screen.height * 3 // 4),
-                                randint(5, 40),
+                                randint(start, stop),
                                 start_frame=randint(0, 250)))
 
     scenes.append(Scene(effects, -1))
