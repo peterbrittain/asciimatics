38e58792a102eee7d828759681e444bb0a3046c8
peterbrittain <peter.brittain.os@gmail.com>
2015-10-06 23:39:50 +0100

    Tidy up Particle System implementation.

--- asciimatics/particles.py
+++ asciimatics/particles.py
@@ -1,10 +1,12 @@
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
+from abc import ABCMeta, abstractmethod
 from builtins import object
 from builtins import range
 from math import pi, sin, cos
 from random import uniform, randint
+from future.utils import with_metaclass
 from asciimatics.effects import Effect
 from asciimatics.screen import Screen
 
@@ -50,27 +52,24 @@ class _Particle(object):
         return self._last
 
 
-class ParticleSystem(Effect):
+class ParticleSystem(object):
     """
     A simple particle system to group together a set of :py:obj:`._Particle`
     objects to create a visual effect.
     """
 
-    def __init__(self, screen, count, new_particle, spawn, life_time, **kwargs):
-        super(ParticleSystem, self).__init__(**kwargs)
+    def __init__(self, screen, x, y, count, new_particle, spawn, life_time):
+        super(ParticleSystem, self).__init__()
         self._screen = screen
+        self._x = x
+        self._y = y
         self._count = count
         self._new_particle = new_particle
-        self._spawn = spawn
         self._life_time = life_time
         self._particles = []
-        self._time_left = 0
+        self._time_left = spawn
 
-    def reset(self):
-        self._particles = []
-        self._time_left = self._spawn
-
-    def _update(self, frame_no):
+    def update(self):
         # Spawn new particles if required
         if self._time_left > 0:
             self._time_left -= 1
@@ -92,6 +91,38 @@ class ParticleSystem(Effect):
             else:
                 self._particles.remove(particle)
 
+    def __str__(self):
+        return "ParticleSystem ({} {} {})".format(
+            self._count, self._life_time, self._time_left)
+
+
+class ParticleEffect(with_metaclass(ABCMeta, Effect)):
+    """
+    An Effect that uses a :py:obj:`.ParticleSystem` to create the animation.
+    """
+
+    def __init__(self, screen, x, y, life_time, **kwargs):
+        super(ParticleEffect, self).__init__(**kwargs)
+        self._screen = screen
+        self._x = x
+        self._y = y
+        self._life_time = life_time
+        self._active_systems = []
+
+    @abstractmethod
+    def reset(self):
+        """
+        Reset the particle effect back to its initial state.
+        """
+
+    def _update(self, frame_no):
+        # Take a copy in case a new system is added to the list this iteration.
+        for system in self._active_systems.copy():
+            if len(system._particles) > 0 or system._time_left > 0:
+                system.update()
+            else:
+                self._active_systems.remove(system)
+
     def stop_frame(self):
         return self._stop_frame
 
@@ -102,13 +133,10 @@ class Rocket(ParticleSystem):
     """
     def __init__(self, screen, x, y, life_time, on_destroy=None):
         super(Rocket, self).__init__(
-            screen, 1, self._new_particle, 1, life_time)
-        self._x = x
-        self._y = screen.height - 1
+            screen, x, screen.height - 1, 1, self._new_particle, 1, life_time)
         self._end_y = y
         self._acceleration = (self._end_y - self._y) // life_time
         self._on_destroy = on_destroy
-        self._life_time = life_time
 
     def _new_particle(self):
         return _Particle("|",
@@ -142,15 +170,13 @@ class RingExplosion(ParticleSystem):
 
     def __init__(self, screen, x, y, life_time):
         super(RingExplosion, self).__init__(
-            screen, 15, self._new_particle, 3, life_time)
-        self._x = x
-        self._y = y
+            screen, x, y, 15, self._new_particle, 3, life_time)
         self._colour = randint(1, 7)
         self._acceleration = 1.0 - (1.0 / life_time)
 
     def _new_particle(self):
         direction = uniform(0, 2 * pi)
-        return _Particle("*+:. ",
+        return _Particle("**::. ",
                          self._x,
                          self._y,
                          sin(direction) * 3 * 8 / self._life_time,
@@ -176,14 +202,12 @@ class RingExplosion(ParticleSystem):
 
 class SerpentExplosion(ParticleSystem):
     """
-    An firework where each trail changes direction.
+    A firework where each trail changes direction.
     """
 
     def __init__(self, screen, x, y, life_time):
         super(SerpentExplosion, self).__init__(
-            screen, 8, self._new_particle, 2, life_time)
-        self._x = x
-        self._y = y
+            screen, x, y, 8, self._new_particle, 2, life_time)
         self._colour = randint(1, 7)
 
     def _new_particle(self):
@@ -218,26 +242,26 @@ class SerpentExplosion(ParticleSystem):
 
 class StarExplosion(ParticleSystem):
     """
-    A classic firework exploding to a star shape.
+    A classic firework exploding to a Peony shape with trails.
     """
 
-    def __init__(self, screen, x, y, life_time):
+    def __init__(self, screen, x, y, life_time, on_each):
         super(StarExplosion, self).__init__(
-            screen, 10, self._new_particle, life_time, life_time)
-        self._x = x
-        self._y = y
+            screen, x, y, 15, self._new_particle, 2, life_time)
         self._colour = randint(1, 7)
         self._acceleration = 1.0 - (1.0 / life_time)
+        self._on_each = on_each
 
     def _new_particle(self):
         direction = randint(0, 16) * pi / 8
-        return _Particle("...++ ",
+        return _Particle("+",
                          self._x,
                          self._y,
                          sin(direction) * 3 * 8 / self._life_time,
                          cos(direction) * 1.5 * 8 / self._life_time,
                          [(self._colour, Screen.A_BOLD, 0), (0, 0, 0)],
-                         self._explode)
+                         self._explode,
+                         on_each=self._on_each)
 
     def _explode(self, particle):
         # Simulate some gravity and slowdown in explosion
@@ -255,100 +279,89 @@ class StarExplosion(ParticleSystem):
                 colour[0], colour[1], colour[2])
 
 
-class StarFirework(Effect):
+class StarTrail(ParticleSystem):
+    """
+    A trail for a StarExplosion.
+    """
+
+    def __init__(self, screen, x, y, life_time, colour):
+        super(StarTrail, self).__init__(
+            screen, x, y, 1, self._new_particle, 1, life_time)
+        self._colour = colour
+
+    def _new_particle(self):
+        return _Particle("+... ",
+                         self._x,
+                         self._y,
+                         0,
+                         0,
+                         [(self._colour, Screen.A_BOLD, 0), (0, 0, 0)],
+                         self._twinkle)
+
+    def _twinkle(self, particle):
+        # Simulate some gravity
+        particle.dy += 0.03
+        particle.y += particle.dy
+
+        colour = particle.colours[
+            (len(particle.colours)-1) * particle.time // self._life_time]
+        return (particle.chars[
+                (len(particle.chars)-1) * particle.time // self._life_time],
+                int(particle.x),
+                int(particle.y),
+                colour[0], colour[1], colour[2])
+
+
+class StarFirework(ParticleEffect):
     """
     Classic rocket with star explosion.
     """
 
     def __init__(self, screen, x, y, life_time, **kwargs):
-        super(StarFirework, self).__init__(**kwargs)
-        self._screen = screen
-        self._x = x
-        self._y = y
-        self._life_time = life_time
-        self._active_systems = []
+        super(StarFirework, self).__init__(screen, x, y, life_time, **kwargs)
 
     def reset(self):
         self._active_systems = [
             Rocket(self._screen, self._x, self._y, 10, on_destroy=self._next)]
-        for system in self._active_systems:
-            system.reset()
 
     def _next(self, parent):
-        explosion = StarExplosion(
-            self._screen, parent.x, parent.y, self._life_time - 10)
-        explosion.reset()
-        self._active_systems.append(explosion)
+        self._active_systems.append(
+            StarExplosion(self._screen, parent.x, parent.y, self._life_time - 10, on_each=self._trail))
 
-    def _update(self, frame_no):
-        for system in self._active_systems:
-            system.update(frame_no)
+    def _trail(self, parent):
+        if randint(0, len(self._active_systems)) < 30:
+            self._active_systems.insert(0,
+                StarTrail(self._screen, parent.x, parent.y, 7, parent.colours[0][0]))
 
-    def stop_frame(self):
-        return self._stop_frame
-
-
-class RingFirework(Effect):
+class RingFirework(ParticleEffect):
     """
     Classic rocket with ring explosion.
     """
 
     def __init__(self, screen, x, y, life_time, **kwargs):
-        super(RingFirework, self).__init__(**kwargs)
-        self._screen = screen
-        self._x = x
-        self._y = y
-        self._life_time = life_time
-        self._active_systems = []
+        super(RingFirework, self).__init__(screen, x, y, life_time, **kwargs)
 
     def reset(self):
         self._active_systems = [
             Rocket(self._screen, self._x, self._y, 10, on_destroy=self._next)]
-        for system in self._active_systems:
-            system.reset()
 
     def _next(self, parent):
-        explosion = RingExplosion(
-            self._screen, parent.x, parent.y, self._life_time - 10)
-        explosion.reset()
-        self._active_systems.append(explosion)
-
-    def _update(self, frame_no):
-        for system in self._active_systems:
-            system.update(frame_no)
-
-    def stop_frame(self):
-        return self._stop_frame
+        self._active_systems.append(
+            RingExplosion(self._screen, parent.x, parent.y, self._life_time - 10))
 
 
-class SerpentFirework(Effect):
+class SerpentFirework(ParticleEffect):
     """
-    Classic rocket with ring explosion.
+    A firework where each trail changes direction.
     """
 
     def __init__(self, screen, x, y, life_time, **kwargs):
-        super(SerpentFirework, self).__init__(**kwargs)
-        self._screen = screen
-        self._x = x
-        self._y = y
-        self._life_time = life_time
-        self._active_systems = []
+        super(SerpentFirework, self).__init__(screen, x, y, life_time, **kwargs)
 
     def reset(self):
         self._active_systems = [
             Rocket(self._screen, self._x, self._y, 10, on_destroy=self._next)]
-        for system in self._active_systems:
-            system.reset()
 
     def _next(self, parent):
-        explosion = SerpentExplosion(
-            self._screen, parent.x, parent.y, self._life_time - 10)
-        explosion.reset()
-        self._active_systems.append(explosion)
-
-    def _update(self, frame_no):
-        for system in self._active_systems:
-            system.update(frame_no)
-
-    def stop_frame(self):
-        return self._stop_frame
+        self._active_systems.append(
+            SerpentExplosion(self._screen, parent.x, parent.y, self._life_time - 10))
