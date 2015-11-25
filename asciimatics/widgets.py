203fd1e8d7998ee5297a99a34a4c6160d8f0c927
peterbrittain <peter.brittain.os@gmail.com>
2015-11-25 23:50:14 +0000

    Add basic editing facilities to prototype widget.

--- asciimatics/widgets.py
+++ asciimatics/widgets.py
@@ -27,7 +27,7 @@ class Frame(Effect):
 
     def _update(self, frame_no):
         for widget in self._widgets:
-            widget.update()
+            widget.update(frame_no)
 
     @property
     def stop_frame(self):
@@ -88,9 +88,11 @@ class Widget(with_metaclass(ABCMeta, object)):
         self._frame = frame
 
     @abstractmethod
-    def update(self):
+    def update(self, frame_no):
         """
         The update method is called whenever this widget needs to redraw itself.
+
+        :param frame_no: The frame number for this screen update.
         """
 
     @abstractmethod
@@ -132,39 +134,128 @@ class TextBox(Widget):
         """
         super(TextBox, self).__init__(label)
         self._text = text
+        self._line = 0
+        self._column = 0
+        self._start_line = 0
+        self._start_column = 0
 
         # @@@TODO: Fix hack!
-        self._x = 0
-        self._y = 0
-        self._width = 80
+        self._x = 20
+        self._y = 5
+        self._width = 40
         self._height = 10
 
-    def update(self):
-        box = Box(self._width, self._height).rendered_text
-        for (i, line) in enumerate(box[0]):
+        # Create box rendered text now.
+        self._box = Box(self._width, self._height).rendered_text
+
+    def update(self, frame_no):
+        # Calculate new visible limits if needed.
+        self._start_line = max(0, max(self._line - self._height + 3,
+                                      min(self._start_line, self._line)))
+        self._start_column = max(0, max(self._column - self._width + 3,
+                                        min(self._start_column, self._column)))
+
+        # Redraw the frame and label if needed.
+        for (i, line) in enumerate(self._box[0]):
             self._frame.screen.paint(
                 line, self._x, self._y + i, transparent=False)
         if self._label is not None:
             self._frame.screen.paint(
                 " {} ".format(self._label), self._x + 2, self._y)
 
-        for i, text in enumerate(self._text.split("\n")):
-            self._frame.screen.print_at(text, self._x + 1, self._y + i + 1)
+        # Render visible portion of the text.
+        for i, text in enumerate(self._value):
+            if self._start_line <= i < self._start_line + self._height - 2:
+                self._frame.screen.print_at(
+                    text[self._start_column:self._start_column + self._width-2],
+                    self._x + 1,
+                    self._y + i + 1 - self._start_line)
+
+        # Since we switch off the standard cursor, we need to emulate our own.
+        cursor = " "
+        if frame_no % 10 < 5:
+            attr = Screen.A_REVERSE
+        else:
+            attr = 0
+            if self._column < len(self.value[self._line]):
+                cursor = self.value[self._line][self._column]
+        self._frame.screen.print_at(
+            cursor,
+            self._x + self._column + 1 - self._start_column,
+            self._y + self._line + 1 - self._start_line,
+            attr=attr)
 
     def reset(self):
-        self._value = self._text
+        # Reset to original data and move to end of the text.
+        self._value = self._text.split("\n")
+        self._line = len(self._value) - 1
+        self._column = len(self._value[self._line])
 
     def process_event(self, event):
         if isinstance(event, KeyboardEvent):
-            self._frame.screen.print_at(str(event.key_code) + "    ", 0, 20)
-            # Special case processing for editing keys
             if event.key_code in [10, 13]:
-                self._text += "\n"
+                # Split and insert line  on CR or LF.
+                self._value.insert(self._line + 1,
+                                   self._value[self._line][self._column:])
+                self._value[self._line] = self._value[self._line][:self._column]
+                self._line += 1
+                self._column = 0
             elif event.key_code == Screen.KEY_BACK:
-                self._text = self._text[:-1]
-            elif event.key_code > 0:
-                self._text += chr(event.key_code)
+                if self._column > 0:
+                    # Delete character in front of cursor.
+                    self._value[self._line] = "".join([
+                        self._value[self._line][:self._column - 1],
+                        self._value[self._line][self._column:]])
+                    self._column -= 1
+                else:
+                    if self._line > 0:
+                        # Join this line with previous
+                        self._line -= 1
+                        self._column = len(self._value[self._line])
+                        self._value[self._line] += self._value.pop(self._line+1)
+            elif event.key_code == Screen.KEY_UP:
+                # Move up one line in text
+                self._line = max(0, self._line - 1)
+                if self._column >= len(self._value[self._line]):
+                    self._column = len(self._value[self._line])
+            elif event.key_code == Screen.KEY_DOWN:
+                # Move down one line in text
+                self._line = min(len(self._value) - 1, self._line + 1)
+                if self._column >= len(self._value[self._line]):
+                    self._column = len(self._value[self._line])
+            elif event.key_code == Screen.KEY_LEFT:
+                # Move left one char, wrapping to previous line if needed.
+                self._column -= 1
+                if self._column < 0:
+                    if self._line > 0:
+                        self._line -= 1
+                        self._column = len(self._value[self._line])
+                    else:
+                        self._column = 0
+            elif event.key_code == Screen.KEY_RIGHT:
+                # Move right one char, wrapping to next line if needed.
+                self._column += 1
+                if self._column > len(self._value[self._line]):
+                    if self._line < len(self._value) - 1:
+                        self._line += 1
+                        self._column = 0
+                    else:
+                        self._column = len(self._value[self._line])
+            elif event.key_code == Screen.KEY_HOME:
+                # Go to the start of this line
+                self._column = 0
+            elif event.key_code == Screen.KEY_END:
+                # Go to the end of this line
+                self._column = len(self._value[self._line])
+            elif event.key_code >= ord(" "):
+                # Insert any visible text at the current cursor position.
+                self._value[self._line] = chr(event.key_code).join([
+                    self._value[self._line][:self._column],
+                    self._value[self._line][self._column:]])
+                self._column += 1
             else:
+                # Ignore any other key press.
                 return event
         else:
+            # Ignore non-keyboard events
             return event
