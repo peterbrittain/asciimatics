7e15eb45b31b8a7a04e6296452571736a405dfdf
Peter Brittain <peter.brittain.os@gmail.com>
2015-12-17 17:35:41 +0000

    Add some more plumbing to the contact DB demo.

--- samples/contact_list.py
+++ samples/contact_list.py
@@ -1,8 +1,8 @@
 from asciimatics.widgets import Frame, ListBox, Layout, Label, Divider, Text, \
-    CheckBox, RadioButtons, Button
+    CheckBox, RadioButtons, Button, TextBox
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
-from asciimatics.exceptions import ResizeScreenError
+from asciimatics.exceptions import ResizeScreenError, NextScene
 import sys
 import sqlite3
 
@@ -44,6 +44,10 @@ class ContactModel(object):
         contacts = self._db.cursor().execute("SELECT name from contacts").fetchall()
         return {"contacts": [x[0] for x in contacts]}
 
+    def get_contact(self):
+        # TODO: Fix up model for reading one contact
+        return {}
+
 
 class ListView(Frame):
     def __init__(self, screen, model):
@@ -59,10 +63,50 @@ class ListView(Frame):
         layout.add_widget(Divider())
         layout2 = Layout([1, 1, 1, 1, 1])
         self.add_layout(layout2)
-        layout2.add_widget(Button("Add"), 1)
-        layout2.add_widget(Button("Edit"), 2)
-        layout2.add_widget(Button("Delete"), 3)
+        layout2.add_widget(Button("Add", self._add), 1)
+        layout2.add_widget(Button("Edit", self._edit), 2)
+        layout2.add_widget(Button("Delete", self._delete), 3)
         self.fix()
+
+    def _add(self):
+        # TODO: Clear out contact view
+        raise NextScene()
+
+    def _edit(self):
+        # TODO: Populate contact view
+        raise NextScene()
+
+    def _delete(self):
+        # TODO: Remove the entry from the model.
+        raise RuntimeError("Not implemented yet!")
+
+
+class ContactView(Frame):
+    def __init__(self, screen, model):
+        super(ContactView, self).__init__(screen,
+                                          model,
+                                          screen.height,
+                                          screen.width)
+        layout = Layout([100])
+        self.add_layout(layout)
+        layout.add_widget(Text("Name:", "name"))
+        layout.add_widget(Text("Address:", "address"))
+        layout.add_widget(Text("Phone number:", "phone"))
+        layout.add_widget(Text("Email address:", "email"))
+        layout.add_widget(TextBox(5, "Notes:", "notes"))
+        layout.add_widget(Divider())
+        layout2 = Layout([1, 1, 1, 1])
+        self.add_layout(layout2)
+        layout2.add_widget(Button("OK", self._save), 0)
+        layout2.add_widget(Button("Cancel", self._cancel), 3)
+        self.fix()
+
+    def _save(self):
+        # TODO: Save back to the model.
+        raise NextScene()
+
+    def _cancel(self):
+        raise NextScene()
 
 
 def demo(screen):
@@ -70,7 +114,7 @@ def demo(screen):
 
     scenes = [
         Scene([ListView(screen, contacts.get_summary())], -1),
-        # Scene([ContactView(screen)], -1)
+        Scene([ContactView(screen, contacts.get_contact())], -1)
     ]
 
     screen.play(scenes, stop_on_resize=True)
