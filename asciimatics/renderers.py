74a74829983cf8a2daceb07cf978d5b5eb9c4bad
peterbrittain <peter.brittain.os@gmail.com>
2016-07-05 21:56:26 +0100

    Quick test for unicode chars

--- asciimatics/renderers.py
+++ asciimatics/renderers.py
@@ -452,16 +452,23 @@ class Box(StaticRenderer):
     Windows and direct access to curses in Linux.
     """
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, unicode=False):
         """
         :param width: The desired width of the box.
         :param height: The desired height of the box.
+        :param unicode: Whether to use unicode box characters or not.
         """
         super(Box, self).__init__()
-        box = "+" + "-" * (width - 2) + "+\n"
-        for _ in range(height - 2):
-            box += "|" + " " * (width - 2) + "|\n"
-        box += "+" + "-" * (width - 2) + "+\n"
+        if unicode:
+            box = "┌" + "─" * (width - 2) + "┐\n"
+            for _ in range(height - 2):
+                box += "│" + " " * (width - 2) + "│\n"
+            box += "└" + "─" * (width - 2) + "┘\n"
+        else:
+            box = "+" + "-" * (width - 2) + "+\n"
+            for _ in range(height - 2):
+                box += "|" + " " * (width - 2) + "|\n"
+            box += "+" + "-" * (width - 2) + "+\n"
         self._images = [box]
 
 
