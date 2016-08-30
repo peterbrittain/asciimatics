139619678bb69089309f93f6bb53b86df0f32f6b
Peter Brittain <peter.brittain.os@gmail.com>
2016-08-31 00:36:59 +0100

    Fix 2/3 compat

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
+from builtins import str
 import unittest
 import os
 import sys
