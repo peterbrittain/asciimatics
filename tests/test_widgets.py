8ac0fd5bafb49fa2cc91134e0ae2a01cccb914c2
Peter Brittain <peter.brittain.os@gmail.com>
2016-08-31 00:35:46 +0100

    Fix 2/3 compat

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
+from builtins import chr
 import unittest
 from mock.mock import MagicMock
 from asciimatics.event import KeyboardEvent, MouseEvent
