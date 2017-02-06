063a280933aaa3911fc99e89486cf26d4c849acd
peterbrittain <pab_user@server.fake>
2017-02-06 18:02:07 +0000

    More frills for "top" demo.

--- samples/top.py
+++ samples/top.py
@@ -11,10 +11,6 @@ except ImportError:
     print("Please run `pip install psutil` and try again.")
     sys.exit(0)
 
-# Text constants for the UI
-FOOTER_L = "Another example from asciimatics"
-FOOTER_R = "written by Peter Brittain"
-
 
 class DemoFrame(Frame):
     def __init__(self, screen):
@@ -25,6 +21,8 @@ class DemoFrame(Frame):
                                         name="My Form")
         # Internal state for doing periodic updates
         self._last_frame = 0
+        self._sort = 5
+        self._reverse = True
         self._list = MultiColumnListBox(Widget.FILL_FRAME,
                                         [6, 10, 3, 10, 10, 5, 5, 100],
                                         [], name="mc_list")
@@ -32,21 +30,13 @@ class DemoFrame(Frame):
 
         # Create the basic form layout...
         layout = Layout([1], fill_frame=True)
-        header = Label("")
-        footer = Label("")
+        self._header = Label("")
+        footer = Label("Press `<`/`>` to change sort, `r` to toggle order, or `q` to quit.")
         self.add_layout(layout)
-        layout.add_widget(header)
+        layout.add_widget(self._header)
         layout.add_widget(self._list)
         layout.add_widget(footer)
         self.fix()
-
-        # Update the dynamic header and footer.
-        # TODO: don't like the access to this internal field.
-        header._text = "  Some info:  86%, 10%, 4%        Some more: 1.2GB"
-        footer._text = "{}{:{}}{}".format(FOOTER_L,
-                                          "",
-                                          max(1, self._canvas.width - len(FOOTER_L) - len(FOOTER_R)),
-                                          FOOTER_R)
 
         # Add my own colour palette
         for key in self.palette:
@@ -55,51 +45,82 @@ class DemoFrame(Frame):
             self.palette[key] = (Screen.COLOUR_WHITE, Screen.A_BOLD, Screen.COLOUR_BLACK)
 
     def process_event(self, event):
-        # Allow for normal handling in alll widgets.
+        # Allow for normal handling in all widgets.
         event = super(DemoFrame, self).process_event(event)
 
         # Now do some global key handling for this Frame.
         if isinstance(event, KeyboardEvent):
-            if event.key_code in [ord('q'), ord('Q')]:
+            if event.key_code in [ord('q'), ord('Q'), Screen.ctrl("c")]:
                 raise StopApplication("User quit")
+            elif event.key_code in [ord("r"), ord("R")]:
+                self._reverse = not self._reverse
+            elif event.key_code == ord("<"):
+                self._sort = max(0, self._sort - 1)
+            elif event.key_code == ord(">"):
+                self._sort = min(7, self._sort + 1)
+
+            # Force a refresh for improved responsiveness
+            self._last_frame = 0
 
     def _update(self, frame_no):
         # Refresh the list view if needed
         if frame_no - self._last_frame >= 20 or self._last_frame == 0:
             self._last_frame = frame_no
 
-            # Create the data to go in the mutli-column list...
+            # Create the data to go in the multi-column list...
             last_selection = self._list.value
             list_data = []
             for process in psutil.process_iter():
                 memory = process.memory_info()
-                # TODO: handle other types better?
                 data = [
-                    str(process.pid),
+                    process.pid,
                     process.username(),
-                    str(process.nice()),
-                    str(memory.vms),
-                    str(memory.rss),
-                    str(round(process.cpu_percent() * 10, 0) / 10),
-                    str(round(process.memory_percent() * 10, 0) / 10),
-                    process.name()
+                    process.nice(),
+                    memory.vms,
+                    memory.rss,
+                    process.cpu_percent(),
+                    process.memory_percent(),
+                    " ".join(process.cmdline()) if process.cmdline() else process.name()
                 ]
-                list_data.append((data, process.pid))
+                list_data.append(data)
 
-            # Apply current sort
-            list_data = sorted(list_data, key=lambda x: x[0][5], reverse=True)
+            # Apply current sort and reformat for humans
+            list_data = sorted(list_data,
+                               key=lambda x: x[self._sort],
+                               reverse=self._reverse)
+            # TODO: handle other types better?
+            new_data = [
+                ([
+                    str(x[0]),
+                    x[1],
+                    str(x[2]),
+                    str(int(x[3] / 1024 / 1024)),
+                    str(int(x[4] / 1024 / 1024)),
+                    str(round(x[5] * 10, 0) / 10),
+                    str(round(x[6] * 10, 0) / 10),
+                    x[7]
+                ], x[0]) for x in list_data
+            ]
 
             # Update the list and try to reset the last selection.
-            self._list.options = list_data
+            self._list.options = new_data
             self._list.value = last_selection
+
+            # TODO: don't like the access to this internal field.
+            # TODO: better options for formatting layouts within a text box?
+            memory = psutil.virtual_memory()
+            self._header._text = (
+                "CPU usage: {}%   Memory available: {}M".format(
+                        str(round(psutil.cpu_percent() * 10, 0) / 10),
+                        str(int(memory.available / 1024 / 1024))))
 
         # Now redraw as normal
         super(DemoFrame, self)._update(frame_no)
 
     @property
     def frame_update_count(self):
-        # TODO: something better to handle this?
-        return 20
+        # Refresh once a second...
+        return 40
 
 
 def demo(screen):
