6d938c52109546fcbe654ea46b41b66dac7660df
Peter Brittain <peter.brittain.os@gmail.com>
2017-05-03 13:20:59 +0100

    Tidy up kaleidoscope demo

--- samples/kaleidoscope.py
+++ samples/kaleidoscope.py
@@ -1,4 +1,5 @@
-from asciimatics.renderers import Kaleidoscope, FigletText, Rainbow, RotatedDuplicate
+from asciimatics.renderers import Kaleidoscope, FigletText, Rainbow, RotatedDuplicate, \
+    StaticRenderer
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.effects import Print
@@ -8,22 +9,34 @@ import sys
 
 def demo(screen):
     scenes = []
+    cell1 = Rainbow(screen,
+                    RotatedDuplicate(screen.width // 2,
+                                     screen.height,
+                                     FigletText("ASCII" if screen.width < 80 else "ASCII rules",
+                                                font="banner",
+                                                width=screen.width // 2)))
+    cell2 = ""
+    for y in range(screen.height):
+        for x in range(screen.width // 2):
+            c = (x + y) % screen.colours
+            cell2 += "${%d,2,%d}:" % (c, c)
+        cell2 += "\n"
     for i in range(8):
         scenes.append(
                 Scene([Print(screen,
-                             Kaleidoscope(screen.height,
-                                          screen.width,
-                                          Rainbow(screen,
-                                                  RotatedDuplicate(screen,
-                                                                   FigletText("ASCII rules",
-                                                                              font="banner",
-                                                                              width=screen.width // 2))),
-                                          i),
+                             Kaleidoscope(screen.height, screen.width, cell1, i),
                              0,
                              speed=1,
                              transparent=False),
                        Print(screen,
                              FigletText(str(i)), screen.height - 6, x=screen.width - 8, speed=1)],
+                      duration=360))
+        scenes.append(
+                Scene([Print(screen,
+                             Kaleidoscope(screen.height, screen.width, StaticRenderer([cell2]), i),
+                             0,
+                             speed=1,
+                             transparent=False)],
                       duration=360))
     screen.play(scenes, stop_on_resize=True)
 
