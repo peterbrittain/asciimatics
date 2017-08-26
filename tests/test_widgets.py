f216193cadee65bd47cec96cbe24a2df7563afde
Peter Brittain <peter.brittain.os@gmail.com>
2017-08-26 17:51:35 +0100

    Fix missing import
    Why did I start editing code in a web browser?!

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -4,11 +4,10 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-
 from mock import patch
-
 from builtins import chr
 import unittest
+import sys
 from mock.mock import MagicMock
 from asciimatics.event import KeyboardEvent, MouseEvent
 from asciimatics.exceptions import NextScene, StopApplication, InvalidFields
