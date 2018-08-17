2e5d64fa90698de64f5953960697e40868dbbf3b
Peter Brittain <peter.brittain.os@gmail.com>
2018-08-17 10:19:30 +0100

    Attempt to solve macos file names

--- asciimatics/widgets.py
+++ asciimatics/widgets.py
@@ -12,6 +12,7 @@ from collections import defaultdict
 from types import FunctionType
 import re
 import os
+import unicodedata
 from builtins import chr
 from builtins import range
 from builtins import object
@@ -2839,36 +2840,32 @@ class FileBrowser(MultiColumnListBox):
         for my_file in files:
             full_path = os.path.join(self._root, my_file)
             details = os.lstat(full_path)
+            name = "|-- {}".format(my_file)
+            tree = tree_files
             if os.path.isdir(full_path):
+                tree = tree_dirs
                 if os.path.islink(full_path):
                     # Show links separately for directories
                     real_path = os.path.realpath(full_path)
-                    tree_dirs.append((["|-+ {} -> {}".format(my_file, real_path),
-                                    "",
-                                    readable_timestamp(details.st_mtime)], full_path))
+                    name = "|-+ {} -> {}".format(my_file, real_path)
                 else:
-                    tree_dirs.append((["|-+ {}".format(my_file),
-                                    "",
-                                    readable_timestamp(details.st_mtime)], full_path))
+                    name = "|-+ {}".format(my_file)
             elif os.path.islink(full_path):
                 # Check if link target exists and if it does, show statistics of the
                 # linked file, otherwise just display the link
                 real_path = os.path.realpath(full_path)
                 if os.path.exists(real_path):
-                    real_details = os.stat(real_path)
-                    tree_files.append((["|-- {} -> {}".format(my_file, real_path),
-                                        readable_mem(real_details.st_size),
-                                        readable_timestamp(real_details.st_mtime)], full_path))
+                    details = os.stat(real_path)
+                    name = "|-- {} -> {}".format(my_file, real_path)
                 else:
                     # Both broken directory and file links fall to this case.
                     # Actually using the files will cause a FileNotFound exception
-                    tree_files.append((["|-- {} -> {}".format(my_file, real_path),
-                                        readable_mem(details.st_size),
-                                        readable_timestamp(details.st_mtime)], full_path))
-            else:
-                tree_files.append((["|-- {}".format(my_file),
-                                    readable_mem(details.st_size),
-                                    readable_timestamp(details.st_mtime)], full_path))
+                    name = "|-- {} -> {}".format(my_file, real_path)
+
+            # Normalize names for MacOS and then add to the list.
+            tree.append(([unicodedata.normalize("NFC", name),
+                          readable_mem(details.st_size),
+                          readable_timestamp(details.st_mtime)], full_path))
 
         tree_view.extend(sorted(tree_dirs))
         tree_view.extend(sorted(tree_files))
