9ae0d75ef284334c34ff3880a215f42339e2471e
Peter Brittain <peter.brittain.os@gmail.com>
2019-08-02 23:21:59 +0100

    Some UTs for ColouredText

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -6,10 +6,11 @@ from __future__ import unicode_literals
 from datetime import datetime
 import time
 import unittest
-from asciimatics.utilities import readable_mem, readable_timestamp
+from asciimatics.utilities import readable_mem, readable_timestamp, ColouredText
+from asciimatics.parsers import AsciimaticsParser
 
 
-class TestWidgets(unittest.TestCase):
+class TestUtilities(unittest.TestCase):
 
     def test_readable_mem(self):
         """
@@ -33,3 +34,36 @@ class TestWidgets(unittest.TestCase):
             time.mktime(datetime.now().replace(hour=0, minute=1, second=2).timetuple())))
         self.assertEquals("1999-01-02", readable_timestamp(
             time.mktime(datetime.now().replace(year=1999, month=1, day=2).timetuple())))
+
+    def test_coloured_text(self):
+        """
+        Check ColouredText works as expected.
+        """
+        # No specified start colour
+        ct = ColouredText("Some ${1}text", AsciimaticsParser())
+        self.assertEquals(str(ct), "Some text")
+        self.assertEquals(ct.raw_text, "Some ${1}text")
+        self.assertEquals(len(ct), 9)
+        self.assertEquals(ct.first_colour, None)
+        self.assertEquals(ct.last_colour, (1, 0, None))
+        self.assertEquals(ct.colour_map[0], (None, None, None))
+        
+        # Specified start colour
+        ct = ColouredText("Some ${1}text", AsciimaticsParser(), colour=(2, 1, 0))
+        self.assertEquals(str(ct), "Some text")
+        self.assertEquals(ct.raw_text, "Some ${1}text")
+        self.assertEquals(len(ct), 9)
+        self.assertEquals(ct.first_colour, (2, 1, 0))
+        self.assertEquals(ct.last_colour, (1, 0, None))
+        self.assertEquals(ct.colour_map[0], (2, 1, 0))
+
+        # Slicing
+        self.assertEquals(ct[0], ColouredText("S", AsciimaticsParser()))
+        self.assertEquals(ct[1:-1], ColouredText("ome ${1}tex", AsciimaticsParser()))
+        self.assertNotEquals(ct[1:-1], ColouredText("ome tex", AsciimaticsParser()))
+
+        # Adding
+        self.assertEquals(
+            ColouredText("Some ", AsciimaticsParser()) +
+            ColouredText("${3}Text", AsciimaticsParser()),
+            ColouredText("Some ${3}Text", AsciimaticsParser()))
