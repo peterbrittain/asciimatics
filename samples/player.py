e029c59562c02222b67cdb65a006b70630df318d
Peter Brittain <peter.brittain.os@gmail.com>
2021-06-02 23:24:42 +0100

    Fix player fruit.ans reference

--- samples/player.py
+++ samples/player.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename="debug.log", level=logging.DEBUG)
 
 def demo(screen, scene):
     with AsciinemaPlayer("test.rec", max_delay=0.1) as player, \
-            AnsiArtPlayer("test.ans", rate=2) as player2:
+            AnsiArtPlayer("fruit.ans") as player2:
         screen.play(
             [
                 Scene(
