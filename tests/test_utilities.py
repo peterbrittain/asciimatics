2692edba3b57f0d66a0b81ffed50fb64e87a2eeb
Peter Brittain <peter.brittain.os@gmail.com>
2023-08-04 14:57:25 +0100

    Fix test deprecations

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -17,33 +17,33 @@ class TestUtilities(unittest.TestCase):
         Check readable_mem works as expected.
         """
         # Check formatting works as expected.
-        self.assertEquals("9999", readable_mem(9999))
-        self.assertEquals("10K", readable_mem(10240))
-        self.assertEquals("1024K", readable_mem(1024*1024))
-        self.assertEquals("10M", readable_mem(1024*1024*10))
-        self.assertEquals("10G", readable_mem(1024*1024*1024*10))
-        self.assertEquals("10T", readable_mem(1024*1024*1024*1024*10))
-        self.assertEquals("10P", readable_mem(1024*1024*1024*1024*1024*10))
+        self.assertEqual("9999", readable_mem(9999))
+        self.assertEqual("10K", readable_mem(10240))
+        self.assertEqual("1024K", readable_mem(1024*1024))
+        self.assertEqual("10M", readable_mem(1024*1024*10))
+        self.assertEqual("10G", readable_mem(1024*1024*1024*10))
+        self.assertEqual("10T", readable_mem(1024*1024*1024*1024*10))
+        self.assertEqual("10P", readable_mem(1024*1024*1024*1024*1024*10))
 
     def test_readable_timestamp(self):
         """
         Check readable_timestamp works as expected.
         """
         # Check formatting works as expected.
-        self.assertEquals("12:01:02AM", readable_timestamp(
+        self.assertEqual("12:01:02AM", readable_timestamp(
             time.mktime(datetime.now().replace(hour=0, minute=1, second=2).timetuple())))
-        self.assertEquals("1999-01-02", readable_timestamp(
+        self.assertEqual("1999-01-02", readable_timestamp(
             time.mktime(datetime.now().replace(year=1999, month=1, day=2).timetuple())))
 
 
     def test_boxtool(self):
         # SINGLE_LINE
         tool = BoxTool(True)
-        self.assertEquals("┌───┐", tool.box_top(5))
-        self.assertEquals("└───┘", tool.box_bottom(5))
-        self.assertEquals("│   │", tool.box_line(5))
+        self.assertEqual("┌───┐", tool.box_top(5))
+        self.assertEqual("└───┘", tool.box_bottom(5))
+        self.assertEqual("│   │", tool.box_line(5))
 
-        self.assertEquals(
+        self.assertEqual(
             tool.box(5, 3),
             "┌───┐\n" +
             "│   │\n" +
@@ -51,11 +51,11 @@ class TestUtilities(unittest.TestCase):
 
         # DOUBLE_LINE
         tool.style = DOUBLE_LINE
-        self.assertEquals("╔═══╗", tool.box_top(5))
-        self.assertEquals("╚═══╝", tool.box_bottom(5))
-        self.assertEquals("║   ║", tool.box_line(5))
+        self.assertEqual("╔═══╗", tool.box_top(5))
+        self.assertEqual("╚═══╝", tool.box_bottom(5))
+        self.assertEqual("║   ║", tool.box_line(5))
 
-        self.assertEquals(
+        self.assertEqual(
             tool.box(5, 3),
             "╔═══╗\n" +
             "║   ║\n" +
@@ -63,11 +63,11 @@ class TestUtilities(unittest.TestCase):
 
         # ASCII_LINE
         tool = BoxTool(False)
-        self.assertEquals("+---+", tool.box_top(5))
-        self.assertEquals("+---+", tool.box_bottom(5))
-        self.assertEquals("|   |", tool.box_line(5))
+        self.assertEqual("+---+", tool.box_top(5))
+        self.assertEqual("+---+", tool.box_bottom(5))
+        self.assertEqual("|   |", tool.box_line(5))
 
-        self.assertEquals(
+        self.assertEqual(
             tool.box(5, 3),
             "+---+\n" +
             "|   |\n" +
